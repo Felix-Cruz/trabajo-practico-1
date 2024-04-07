a1eb1567d7f3de13108b04869aca07c53624a4c4
ElMis <ElMis@felix>
2024-04-06 22:33:36 -0300

    Se agrega otro paquete y clase para el ejercicio 1

---
+++ tp1_CruzFelix/src/ar/edu/unju/fi/ejercicio1/model/Model.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.ejercicio1.model;
+
+public class Model {
+	private int numero1;
+	private int numero2;
+	private int numero3;
+	private int numero4;
+	private int numero5;
+	
+	
+	
+	
+}
