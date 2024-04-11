dd4b9d4ee67751e0092e16f4a79f51354e6957d0
ElMis <ElMis@felix>
2024-04-11 16:29:29 -0300

    Se agrega el paquete y clase para el ejercicio 4

--- tp1_CruzFelix/src/ar/edu/unju/fi/ejercicio4/Main.java
+++ tp1_CruzFelix/src/ar/edu/unju/fi/ejercicio4/Main.java
@@ -1,10 +1,26 @@
 package ar.edu.unju.fi.ejercicio4;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ingrese numero: ");
+		int num = scanner.nextInt();
+		int factorial= 1;
+		if ((num <=10) && (num>=0)) {
+			while (1<num) {
+				factorial= factorial*num;
+				num--;
+			} 
+			System.out.println("El factorial es: "+factorial);
+		} else {
+				System.out.println("El numero tiene que ser entre 0 y 10");
+			}
+		
+		scanner.close();
 	}
 
 }
